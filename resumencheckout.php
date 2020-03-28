efa001ef30a0d5b815c94a67f0b7d4c123940b84
tepchin <tepchin@hotmail.com>
2020-03-28 15:34:25 -0600

    Update resumencheckout.php

--- resumencheckout.php
+++ resumencheckout.php
@@ -1,8 +1,25 @@
 <?php
 
+// SDK de Mercado Pago
+require __DIR__ .  '/vendor/autoload.php';
+
+
+// Agrega credenciales
+MercadoPago\SDK::setAccessToken('APP_USR-6718728269189792-112017-dc8b338195215145a4ec035fdde5cedf-491494389');
+
+
 while ($post = each($_POST))
 {
 echo $post[0] . " = " . $post[1]."<br>";
 } 
 
+
+
 ?>
+
+<form action="/procesar-pago" method="POST">
+  <script
+   src="https://www.mercadopago.com.mx/integrations/v1/web-payment-checkout.js"
+   data-preference-id="<?php echo $preference->id; ?>">
+  </script>
+</form>
