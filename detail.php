f7f04704818e9cd37e754ee4278e2d19be75dc92
Israel <tepchin@hotmail.com>
2020-03-28 19:42:23 -0600

    modificacion del formulario

--- detail.php
+++ detail.php
@@ -44,100 +44,107 @@
 <body class="as-theme-light-heroimage">
 
     <div class="stack">
-        
-        <div class="as-search-wrapper" role="main">
-            <div class="as-navtuck-wrapper">
-                <div class="as-l-fullwidth  as-navtuck" data-events="event52">
-                    <div>
-                        <div class="pd-billboard pd-category-header">
-                            <div class="pd-l-plate-scale">
-                                <div class="pd-billboard-background">
-                                    <img src="./assets/music-audio-alp-201709" alt="" width="1440" height="320" data-scale-params-2="wid=2880&amp;hei=640&amp;fmt=jpeg&amp;qlt=95&amp;op_usm=0.5,0.5&amp;.v=1503948581306" class="pd-billboard-hero ir">
-                                </div>
-                                <div class="pd-billboard-info">
-                                    <h1 class="pd-billboard-header pd-util-compact-small-18">Tienda e-commerce</h1>
+        <form method="post" action ='resumencheckout.php' id='Forma' name='Forma'>
+            <div class="as-search-wrapper" role="main">
+                <div class="as-navtuck-wrapper">
+                    <div class="as-l-fullwidth  as-navtuck" data-events="event52">
+                        <div>
+                            <div class="pd-billboard pd-category-header">
+                                <div class="pd-l-plate-scale">
+                                    <div class="pd-billboard-background">
+                                        <img src="./assets/music-audio-alp-201709" alt="" width="1440" height="320" data-scale-params-2="wid=2880&amp;hei=640&amp;fmt=jpeg&amp;qlt=95&amp;op_usm=0.5,0.5&amp;.v=1503948581306" class="pd-billboard-hero ir">
+                                    </div>
+                                    <div class="pd-billboard-info">
+                                        <h1 class="pd-billboard-header pd-util-compact-small-18">Tienda e-commerce</h1>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="as-search-results as-filter-open as-category-landing as-desktop" id="as-search-results">
+                <div class="as-search-results as-filter-open as-category-landing as-desktop" id="as-search-results">
 
-                <div id="accessories-tab" class="as-accessories-details">
-                    <div class="as-accessories" id="as-accessories">
-                        <div class="as-accessories-header">
-                            <div class="as-search-results-count">
-                                <span class="as-search-results-value"></span>
+                    <div id="accessories-tab" class="as-accessories-details">
+                        <div class="as-accessories" id="as-accessories">
+                            <div class="as-accessories-header">
+                                <div class="as-search-results-count">
+                                    <span class="as-search-results-value"></span>
+                                </div>
                             </div>
-                        </div>
-                        <div class="as-searchnav-placeholder" style="height: 77px;">
-                            <div class="row as-search-navbar" id="as-search-navbar" style="width: auto;">
-                                <div class="as-accessories-filter-tile column large-6 small-3">
+                            <div class="as-searchnav-placeholder" style="height: 77px;">
+                                <div class="row as-search-navbar" id="as-search-navbar" style="width: auto;">
+                                    <div class="as-accessories-filter-tile column large-6 small-3">
 
-                                    <button class="as-filter-button" aria-expanded="true" aria-controls="as-search-filters" type="button">
-                                        <h2 class=" as-filter-button-text">
-                                            Smartphones
-                                        </h2>
-                                    </button>
+                                        <button class="as-filter-button" aria-expanded="true" aria-controls="as-search-filters" type="button">
+                                            <h2 class=" as-filter-button-text">
+                                                Smartphones
+                                            </h2>
+                                        </button>
 
+
+                                    </div>
 
                                 </div>
-
                             </div>
-                        </div>
-                        <div class="as-accessories-results  as-search-desktop">
-                            <div class="width:60%">
-                                <div class="as-producttile-tilehero with-paddlenav " style="float:left;">
-                                    <div class="as-dummy-container as-dummy-img">
+                            <div class="as-accessories-results  as-search-desktop">
+                                <div class="width:60%">
+                                    <div class="as-producttile-tilehero with-paddlenav " style="float:left;">
+                                        <div class="as-dummy-container as-dummy-img">
 
-                                        <img src="./assets/wireless-headphones" class="ir ir item-image as-producttile-image  " style="max-width: 70%;max-height: 70%;"alt="" width="445" height="445">
-                                    </div>
-                                    <div class="images mini-gallery gal5 ">
-                                    
+                                            <img src="./assets/wireless-headphones" class="ir ir item-image as-producttile-image  " style="max-width: 70%;max-height: 70%;"alt="" width="445" height="445">
+                                        </div>
+                                        <div class="images mini-gallery gal5 ">
+                                        
 
-                                        <div class="as-isdesktop with-paddlenav with-paddlenav-onhover">
-                                            <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
-                                                <div class="as-tilegallery-element as-image-selected">
-                                                    <div class=""></div>
-                                                    <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
+                                            <div class="as-isdesktop with-paddlenav with-paddlenav-onhover">
+                                                <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
+                                                    <div class="as-tilegallery-element as-image-selected">
+                                                        <div class=""></div>
+                                                        <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
+                                                    </div>
+                                                    
                                                 </div>
+
                                                 
                                             </div>
 
                                             
+
                                         </div>
 
-                                        
-
                                     </div>
+                                    <div class="as-producttile-info" style="float:left;min-height: 168px;">
+                                        <div class="as-producttile-titlepricewraper" style="min-height: 128px;">
+                                            <div class="as-producttile-title">
+                                                <h3 class="as-producttile-name">
+                                                    <p class="as-producttile-tilelink">
+                                                        <span data-ase-truncate="2"><?php echo $_POST['title'] ?></span>
+                                                    </p>
 
-                                </div>
-                                <div class="as-producttile-info" style="float:left;min-height: 168px;">
-                                    <div class="as-producttile-titlepricewraper" style="min-height: 128px;">
-                                        <div class="as-producttile-title">
-                                            <h3 class="as-producttile-name">
-                                                <p class="as-producttile-tilelink">
-                                                    <span data-ase-truncate="2"><?php echo $_POST['title'] ?></span>
-                                                </p>
-
+                                                </h3>
+                                            </div>
+                                            <h3 >
+                                                <?php echo $_POST['price'] ?>
                                             </h3>
+                                            <h3 >
+                                                <?php echo "$" . $_POST['unit'] ?>
+                                            </h3>
+                                            <input type='hidden' name="cantidad" value='1'>
+                                            <input type='hidden' name="descripcion" value='Dispositivo móvil de Tienda e-commerce'>
+                                            <input type='hidden' name="producto" value='<?php echo $_POST['title'] ?>'>
+                                            <input type='hidden' name="imagen"   value='<?php echo $_POST['img'] ?>'>
+                                            <input type='hidden' name="precio"   value='<?php echo $_POST['price'] ?>'>
+                                            
                                         </div>
-                                        <h3 >
-                                            <?php echo $_POST['price'] ?>
-                                        </h3>
-                                        <h3 >
-                                            <?php echo "$" . $_POST['unit'] ?>
-                                        </h3>
+                                        <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
                                     </div>
-                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
         <div role="alert" class="as-loader-text ally" aria-live="assertive"></div>
         <div class="as-footnotes">
             <div class="as-footnotes-content">
